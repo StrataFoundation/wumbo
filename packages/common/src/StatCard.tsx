b7150be3c066efaa5faac4e9e036ef936ded676e
Chewing Glass <chewinglass123@gmail.com>
2021-12-17 17:58:03 -0600

    Style

--- packages/common/src/StatCard.tsx
+++ packages/common/src/StatCard.tsx
@@ -20,87 +20,99 @@ interface IStatCardProps extends FlexProps {
 }
 
 const TagGradients: Record<TokenTier, string> = {
-  "Platinum": "conic-gradient(from -0.22deg at 48.39% 48.72%, #7C9DBF 0deg, #A3D0E6 138.61deg, #40679C 207.87deg, #4D6F88 257.83deg, #576D88 360deg), linear-gradient(185.95deg, #824C17 -3.23%, #D6B23F 5.3%, #FCFBE6 18.62%, #FDFEF9 24.45%, #FCFBE6 31.65%, #E6C970 55.56%, #F8E39F 67.64%, #AC874D 81.76%);",
-  "Default": "green.500"
-}
+  Platinum:
+    "conic-gradient(from -0.22deg at 48.39% 48.72%, #7C9DBF 0deg, #A3D0E6 138.61deg, #40679C 207.87deg, #4D6F88 257.83deg, #576D88 360deg), linear-gradient(185.95deg, #824C17 -3.23%, #D6B23F 5.3%, #FCFBE6 18.62%, #FDFEF9 24.45%, #FCFBE6 31.65%, #E6C970 55.56%, #F8E39F 67.64%, #AC874D 81.76%);",
+  Default: "green.500",
+};
 
-export const StatCard = ({ _hover, tag, tier, label, value, ...flexProps }: IStatCardProps) => {
-  const Inner = <Flex
-    justifyContent="space-between"
-    flexDir="column"
-    w="full"
-    h="full"
-    bgColor="gray.100"
-    padding={3}
-    flexGrow={1}
-    rounded="lg"
-    {...flexProps}
-  >
-    <Text fontSize="xs" color="gray.500">
-      {label}
-    </Text>
-    <Text fontSize="md">{value}</Text>
-  </Flex>;
+export const StatCard = ({
+  _hover,
+  tag,
+  tier,
+  label,
+  value,
+  ...flexProps
+}: IStatCardProps) => {
+  const Inner = (
+    <Flex
+      justifyContent="space-between"
+      flexDir="column"
+      w="full"
+      h="full"
+      bgColor="gray.100"
+      padding={3}
+      flexGrow={1}
+      rounded="lg"
+      {...flexProps}
+    >
+      <Text fontSize="xs" color="gray.500">
+        {label}
+      </Text>
+      <Text fontSize="md">{value}</Text>
+    </Flex>
+  );
 
   if (tier) {
-    return <Box
-      _hover={_hover}
-      position="relative"
-      flexGrow={1}
-      w="full"
-      borderRadius="9px"
-      padding={"1px"}
-      background={getTierGradient(tier) || "gray.100"}
-    >
-      {Inner}
-      {tag &&
-        <>
-          <Box
-            minWidth="30px"
-            h="16px"
-            top="0"
-            right="0"
-            borderBottomLeftRadius="9px"
-            borderTopRightRadius="9px"
-            transform="rotate(-180deg)"
-            position="absolute"
-            background={TagGradients[tier] || "gray.300"}
-          >
-            {/* Purely to get the width right */}
+    return (
+      <Box
+        _hover={_hover}
+        position="relative"
+        flexGrow={1}
+        w="full"
+        borderRadius="9px"
+        padding={"1px"}
+        background={getTierGradient(tier) || "gray.100"}
+      >
+        {Inner}
+        {tag && (
+          <>
+            <Box
+              minWidth="30px"
+              h="16px"
+              top="0"
+              right="0"
+              borderBottomLeftRadius="9px"
+              borderTopRightRadius="9px"
+              transform="rotate(-180deg)"
+              position="absolute"
+              background={TagGradients[tier] || "gray.300"}
+            >
+              {/* Purely to get the width right */}
+              <Text
+                paddingTop="1px"
+                paddingLeft="6px"
+                paddingRight="8px"
+                fontSize="10px"
+                fontWeight="900"
+                opacity={0}
+              >
+                {tag}
+              </Text>
+            </Box>
             <Text
+              minWidth="30px"
               paddingTop="1px"
               paddingLeft="6px"
               paddingRight="8px"
+              textAlign="center"
+              h="16px"
               fontSize="10px"
               fontWeight="900"
-              opacity={0}
+              top="0px"
+              right="0px"
+              color={TagGradients[tier] ? "white" : "gray.500"}
+              position="absolute"
             >
-              { tag }
+              {tag}
             </Text>
-          </Box>
-          <Text
-            minWidth="30px"
-            paddingTop="1px"
-            paddingLeft="6px"
-            paddingRight="8px"
-            textAlign="center"
-            h="16px"
-            fontSize="10px"
-            fontWeight="900"
-            top="0px"
-            right="0px"
-            color={TagGradients[tier] ? "white" : "gray.500"}
-            position="absolute"
-          >
-            {tag}
-          </Text>
-        </>
-      }
-    </Box>
+          </>
+        )}
+      </Box>
+    );
   }
 
-  return Inner
-}
+  return Inner;
+};
 
 interface IStatCardWithIconProps extends IStatCardProps {
   icon: "coin" | "wumbo";
--- packages/common/src/hooks/useTokenTier.tsx
+++ packages/common/src/hooks/useTokenTier.tsx
@@ -1,32 +1,39 @@
 import { NATIVE_MINT } from "@solana/spl-token";
 import { PublicKey } from "@solana/web3.js";
-import { useBondingPricing, usePriceInUsd, useSolPrice } from "@strata-foundation/react";
+import {
+  useBondingPricing,
+  usePriceInUsd,
+  useSolPrice,
+} from "@strata-foundation/react";
 
 export type TokenTier = "Platinum" | "Gold" | "Silver";
 
 export const TierCutoffs: Record<TokenTier, number> = {
-  "Platinum": 1_000_000,
-  "Gold": 100_000,
-  "Silver": 10_000
-}
+  Platinum: 1_000_000,
+  Gold: 100_000,
+  Silver: 10_000,
+};
 
-const TierGradients:  Record<TokenTier, string> = {
-  "Platinum": "linear-gradient(115.09deg, #BEDBEA 3.94%, #77ACCA 24.55%, #06141D 48.17%, #2FCFFF 86.38%)",
-  "Gold": "",
-  "Silver": ""
-}
+const TierGradients: Record<TokenTier, string> = {
+  Platinum:
+    "linear-gradient(115.09deg, #BEDBEA 3.94%, #77ACCA 24.55%, #06141D 48.17%, #2FCFFF 86.38%)",
+  Gold: "",
+  Silver: "",
+};
 export function getTierGradient(tier: TokenTier): string {
-  return TierGradients[tier]
+  return TierGradients[tier];
 }
 
-export function useTokenTier(tokenBonding: PublicKey | undefined | null): TokenTier | undefined {
-  const { pricing } = useBondingPricing(tokenBonding)
+export function useTokenTier(
+  tokenBonding: PublicKey | undefined | null
+): TokenTier | undefined {
+  const { pricing } = useBondingPricing(tokenBonding);
   const fiatPrice = useSolPrice();
-  const locked = (pricing?.locked(NATIVE_MINT) || 0) * (fiatPrice || 0)
+  const locked = (pricing?.locked(NATIVE_MINT) || 0) * (fiatPrice || 0);
 
-  const found = Object.entries(TierCutoffs).reverse().find(([tier, value]) => 
-  locked && locked >= value
-  )
+  const found = Object.entries(TierCutoffs)
+    .reverse()
+    .find(([tier, value]) => locked && locked >= value);
 
   return found && (found[0] as TokenTier);
 }
