a30e3a1a6a2ee6c3130e96941660c74727e84760
Bryan Zettler <bryanzettler@gmail.com>
2021-12-27 10:26:34 -0600

    added ga and cleaned up some console errors

--- packages/site/src/components/Site/components/common/DownloadButton.tsx
+++ packages/site/src/components/Site/components/common/DownloadButton.tsx
@@ -1,26 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { Button, ButtonProps } from "@chakra-ui/react";
 
 interface IDownloadButtonProps extends ButtonProps {}
 
 export const DownloadButton: React.FC<IDownloadButtonProps> = ({ ...rest }) => (
-  <Link
-    to={{
-      pathname:
-        "https://chrome.google.com/webstore/detail/wumbo/opmfbcncfajkanpggglkgabbfhebgikk",
-    }}
-    target="_blank"
+  <Button
+    colorScheme="green"
+    py="4"
+    px="4"
+    lineHeight="1"
+    size="md"
+    onClick={() =>
+      window.open(
+        "https://chrome.google.com/webstore/detail/wumbo/opmfbcncfajkanpggglkgabbfhebgikk"
+      )
+    }
+    {...rest}
   >
-    <Button
-      colorScheme="green"
-      py="4"
-      px="4"
-      lineHeight="1"
-      size="md"
-      {...rest}
-    >
-      Download Wumbo Now
-    </Button>
-  </Link>
+    Download Wumbo Now
+  </Button>
 );
