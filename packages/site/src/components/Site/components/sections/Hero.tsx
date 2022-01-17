0d40d90ec1d7e551e843b689a6bf673b41fc8a22
Chewing Glass <chewinglass123@gmail.com>
2022-01-17 09:42:51 -0600

    Style

--- packages/site/src/components/Site/components/sections/Hero.tsx
+++ packages/site/src/components/Site/components/sections/Hero.tsx
@@ -43,7 +43,8 @@ export const Hero: React.FC = () => {
               color="white"
               textAlign={["center", "center", "left", "left"]}
             >
-              The power to support your favorite artists and influencers is now in your hands{" "}
+              The power to support your favorite artists and influencers is now
+              in your hands{" "}
               <Text as="span" fontWeight="light">
                 with Wumbo.
               </Text>
@@ -57,7 +58,9 @@ export const Hero: React.FC = () => {
               lineHeight={1.5}
               textAlign={["center", "center", "left", "left"]}
             >
-              Wumbo is a Browser Extension that sits on top of Twitter and lets you mint tokens for your favorite creators. Build a thriving community and join them on their journey to the top!
+              Wumbo is a Browser Extension that sits on top of Twitter and lets
+              you mint tokens for your favorite creators. Build a thriving
+              community and join them on their journey to the top!
             </Heading>
             <DownloadButton />
           </Stack>
