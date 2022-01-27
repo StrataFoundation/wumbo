b0b2de4354a000d7653a95fc1cc9a37513012029
Chewing Glass <chewinglass123@gmail.com>
2022-01-26 18:17:54 -0600

    Site hotfix for mpl missing

--- packages/site/config-overrides.js
+++ packages/site/config-overrides.js
@@ -20,11 +20,11 @@ const supportMjs = () => (webpackConfig) => {
       ...webpackConfig.resolve,
       alias: {
         react: path.resolve("../../node_modules/react"),
+        "@solana/wallet-adapter-react": path.resolve("../../node_modules/@solana/wallet-adapter-react"),
         "@toruslabs/solana-embed": path.resolve("../../node_modules/@toruslabs/solana-embed"),
         // For local dev with linked packages:
         ...process.env.LINKED_DEV ? {
           "@chakra-ui/react": path.resolve("../../node_modules/@chakra-ui/react"),
-          "@solana/wallet-adapter-react": path.resolve("../../node_modules/@solana/wallet-adapter-react"),
           "@strata-foundation/react": path.resolve("./node_modules/@strata-foundation/react"),
           "@strata-foundation/spl-token-bonding": path.resolve("./node_modules/@strata-foundation/spl-token-bonding"),
           "@strata-foundation/spl-utils": path.resolve(
