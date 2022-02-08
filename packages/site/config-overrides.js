6d0c5f46db624c6dfbff6438a261dc0093f3018e
Bryan Zettler <bryanzettler@gmail.com>
2022-02-08 12:36:53 -0600

    next spring cleaning

--- packages/site/config-overrides.js
+++ packages/site/config-overrides.js
@@ -11,29 +11,41 @@ const emptyFs = () => (webpackConfig) => ({
 });
 
 const supportMjs = () => (webpackConfig) => {
-    webpackConfig.module.rules.push({
-        test: /\.mjs$/,
-        include: /node_modules/,
-        type: "javascript/auto",
-    });
-    webpackConfig.resolve = {
-      ...webpackConfig.resolve,
-      alias: {
-        react: path.resolve("../../node_modules/react"),
-        "@solana/wallet-adapter-react": path.resolve("../../node_modules/@solana/wallet-adapter-react"),
-        "@toruslabs/solana-embed": path.resolve("../../node_modules/@toruslabs/solana-embed"),
-        // For local dev with linked packages:
-        ...process.env.LINKED_DEV ? {
-          "@chakra-ui/react": path.resolve("../../node_modules/@chakra-ui/react"),
-          "@strata-foundation/react": path.resolve("./node_modules/@strata-foundation/react"),
-          "@strata-foundation/spl-token-bonding": path.resolve("./node_modules/@strata-foundation/spl-token-bonding"),
-          "@strata-foundation/spl-utils": path.resolve(
-            "./node_modules/@strata-foundation/spl-utils"
-          ),
-        } : {}
-      }
-    }
-    return webpackConfig;
+  webpackConfig.module.rules.push({
+    test: /\.mjs$/,
+    include: /node_modules/,
+    type: "javascript/auto",
+  });
+  webpackConfig.resolve = {
+    ...webpackConfig.resolve,
+    alias: {
+      react: path.resolve("../../node_modules/react"),
+      "@solana/wallet-adapter-react": path.resolve(
+        "../../node_modules/@solana/wallet-adapter-react"
+      ),
+      "@toruslabs/solana-embed": path.resolve(
+        "../../node_modules/@toruslabs/solana-embed"
+      ),
+      // For local dev with linked packages:
+      ...(process.env.LINKED_DEV
+        ? {
+            "@chakra-ui/react": path.resolve(
+              "../../node_modules/@chakra-ui/react"
+            ),
+            "@strata-foundation/react": path.resolve(
+              "./node_modules/@strata-foundation/react"
+            ),
+            "@strata-foundation/spl-token-bonding": path.resolve(
+              "./node_modules/@strata-foundation/spl-token-bonding"
+            ),
+            "@strata-foundation/spl-utils": path.resolve(
+              "./node_modules/@strata-foundation/spl-utils"
+            ),
+          }
+        : {}),
+    },
+  };
+  return webpackConfig;
 };
 
 module.exports = override(emptyFs(), supportMjs());
