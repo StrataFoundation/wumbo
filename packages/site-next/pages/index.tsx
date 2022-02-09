87534e2b3570e9ab80e50c5bfaea07d2aac30131
Bryan Zettler <bryanzettler@gmail.com>
2022-02-09 17:50:04 -0600

    seo done, commiting for the night

--- packages/site-next/pages/index.tsx
+++ packages/site-next/pages/index.tsx
@@ -16,15 +16,30 @@ import {
 } from "@/components/index";
 
 const Home: NextPage = () => {
+  const seoDescription =
+    "Wumbo is a Browser Extension that sits on top of Twitter and lets you mint tokens for your favorite creators.";
+
   return (
     <Box>
       <Head>
         <title>Wum.bo</title>
-        <meta
-          name="description"
-          content="Wumbo is a Browser Extension that sits on top of Twitter and lets you mint tokens for your favorite creators."
-        />
         <link rel="icon" href="/favicon.svg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:type" content="website" />
+        <meta name="description" content={seoDescription} />
+        <meta property="og:title" content="Wum.bo" />
+        <meta property="og:image" content="/seo/splash_head_image.png" />
+        <meta property="og:description" content={seoDescription} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="wum.bo" />
+        <meta property="twitter:url" content={`https://wum.bo`} />
+        <meta name="twitter:title" content="Wum.bo" />
+        <meta name="twitter:description" content={seoDescription} />
+        <meta
+          name="twitter:image"
+          content="http://wumbotest.ngrok.io/seo/splash_head_image.png"
+        />
       </Head>
 
       <Box>
